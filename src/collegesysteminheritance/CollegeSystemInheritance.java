0f68d20cd9744b8e6d5107f99dc9abd8c730ab45
macstudent <macstudent@10.10.150.70>
2018-06-05 16:53:56 -0400

    Vehicle file completed

--- src/collegesysteminheritance/CollegeSystemInheritance.java
+++ src/collegesysteminheritance/CollegeSystemInheritance.java
@@ -15,7 +15,14 @@ public class CollegeSystemInheritance {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Vehicle v1 = new Vehicle();
+        v1.display();
+        
+        Cars c1 = new Cars();
+        c1.display();
+        
+        Motorcycle m1 = new Motorcycle();
+        m1.display();
     }
     
 }
