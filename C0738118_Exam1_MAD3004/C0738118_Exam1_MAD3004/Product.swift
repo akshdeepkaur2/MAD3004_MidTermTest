5d11e4a096b1525d441dea44b47cd2b537661828
MacStudent <macstudent@20x-imacxx-m-448.local>
2018-07-23 16:40:37 -0400

    product update

--- C0738118_Exam1_MAD3004/C0738118_Exam1_MAD3004/Product.swift
+++ C0738118_Exam1_MAD3004/C0738118_Exam1_MAD3004/Product.swift
@@ -43,14 +43,84 @@ init(ProductId: Int,ProductName: String,Price: Double,Quantity: Int){
     self.Quantity = Quantity
     
 }
-
-func printData()
+var productDetail: (String) -> String
 {
-    print("ProductId: \(self.ProductId!)")
-    print("ProductName: \(self.ProductName!)")
-    print("Price: \(self.Price!)")
-    print("Quantity: \(self.Quantity!)")
+    
+    var HardDisk: String!
+    {
+        didSet
+        {
+            print(HardDisk)
+        }
+    }
+    var zipDrive: String!
+    {
+        didSet
+        {
+            print(zipDrive)
+        }
+    }
+    
+    
+    var FllopyDisk: String!
+    {
+        didSet
+        {
+            print(FloopyDisk)
+            
+        }
+    }
+    var Monitor: String!
+    {
+        didSet
+        {
+            print(Monitor)
+            
+        }
+    }
+    
+    var iPhone: String!
+    {
+        didSet
+        {
+            print(iPhone)
+            
+        }
+    }
+    var joystick: String!
+    {
+        didSet
+        {
+            print(joystick)
+            
+        }
+    }
+    
+    
+    
+    func  setData (name: String, Id: Int, price: Double, quantity: Double)
+    {
+        
+        
+        self.ProductId = ProductId
+        self.ProductName = ProductName
+        self.Price = Price
+        self.Quantity = Quantity
+    }
+    
+    
+    var ProductDetail  = ["Product Name: [",HardDisk,",",Zip Drive","Monitor","Floppy Disk","iPhone" "joystick]
+        _ = [ [100,200,300,400,500,600] ]
+        _ = [120.00,90.00,100,190,300,700]
+        _ = [2,6,4,3]
+    
+    func display()
+    {
+        print("Order id is: \(self.Id) \t OrderName is: \(self.name?) \t Price is: \(self.price?) \t Quantity is: \(self.quantity?)")
+    }
 }
+
+
 
 
 
